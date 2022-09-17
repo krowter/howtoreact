507df74af1c058738792ebd78cb02c7b4f0c2beb
Krishna Rowter <krisha.rowter@gmail.com>
2022-09-17 19:46:24 +0700

    feat: update question page meta and UI

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,16 +1,29 @@
 import { GetStaticProps } from "next";
 import ReactMarkdown from "react-markdown";
+import Head from "next/head";
 
 import { getAllQuestions } from "services/getAllQuestions";
 import { getQuestionBySlug } from "services/getQuestionBySlug";
 import { Question } from "types/content";
+
+import styles from "styles/question.module.css";
 
 type QuestionBySlugProps = {
   question: Question;
 };
 
 export default function QuestionBySlug(props: QuestionBySlugProps) {
-  return <ReactMarkdown>{props.question.content}</ReactMarkdown>;
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>HowToReact - {props.question.data.indexes[0]}</title>
+        <meta name="description" content={props.question.data.indexes[0]} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <ReactMarkdown>{props.question.content}</ReactMarkdown>
+    </div>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async function ({ params }) {
